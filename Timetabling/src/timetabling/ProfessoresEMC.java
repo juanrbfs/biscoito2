04351a68018a051662843d52eb54f42f3414047f
HOME <HOME@HOME-PC>
2017-04-09 21:33:15 -0300

    Alguns Testes

---
+++ Timetabling/src/timetabling/ProfessoresEMC.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetabling;
+
+/**
+ *
+ * @author HOME
+ */
+public class ProfessoresEMC {
+    
+    public static int ADALBERTO        = 1;
+    public static int ADMYR            = 2;
+    public static int ADRIANO          = 3;
+    public static int ANA_CLAUDIA      = 4;
+    public static int ANDREIA          = 5;
+    public static int ANTONIO_BALEEIRO = 6;
+    public static int ANTONIO_MELO     = 7;
+    public static int BERNARDO         = 8;
+    public static int CACILDA          = 9;
+    public static int CARLOS_GALVAO    = 10;
+    public static int CASSIO_VINHAL    = 11;
+    public static int COLEMAR          = 12;
+    public static int DEMOSTENES       = 13;
+    public static int EMILSON          = 14;
+    public static int ENES             = 15;
+    public static int EULER            = 16;
+    public static int FERNANDO_MELO    = 17;
+    public static int FLAVIO           = 18;
+    public static int GELSON_BRIGATTO  = 19;
+    public static int GELSON_CRUZ      = 20;
+    public static int GETULIO          = 21;
+    public static int GEYVERSON        = 22;
+    public static int GISELE           = 23;
+    public static int IGOR_KOPCAK      = 24;
+    public static int JOAO_PAULO       = 25;
+    public static int JOSE_WILSON      = 26;
+    public static int KARINA           = 27;
+    public static int KLEBER_FIGUEIREDO= 28;
+    public static int LEONARDO_BRITO   = 29;
+    public static int LEONARDO_QUEIROZ = 30;
+    public static int LEONARDO_GUEDES  = 31;
+    public static int LINA_PAOLA       = 32;
+    public static int LOURENCO         = 33;
+    public static int MARA_GRACE       = 34;
+    public static int MARCELO_STEHLING = 35;
+    public static int MARCO_ASSFALK    = 36;
+    public static int MARCOS_SOUZA     = 37;
+    public static int MARLIPE          = 38;
+    public static int REINALDO         = 39;
+    public static int RODRIGO_LEMOS    = 40;
+    public static int ROSANGELA        = 41;
+    public static int SANDRERLEY       = 42;
+    public static int SERGIO_FIGUEIREDO= 43;
+    public static int SERGIO_GRANATO   = 44;
+    public static int SERGIO_PIMENTEL  = 45;
+    public static int SIGIO            = 46;
+    public static int THYAGO_MARQUES   = 47;
+    public static int TOMAS_BADAN      = 48;
+    public static int WANDER           = 49;
+    public static int WANIR            = 50;
+    public static int WEBER            = 51;
+    
+    public static String _ADALBERTO        = "Adalberto José Batista";
+    public static String _ADMYR            = "Ademyr Gonçalves de Oliveira";
+    public static String _ADRIANO          = "Adriano César Santana";
+    public static String _ANA_CLAUDIA      = "Ana Cláudia Marques do Valle";
+    public static String _ANDREIA          = "Andreia Aoyagui Nascimento";
+    public static String _ANTONIO_BALEEIRO = "Antônio César Baleeiro Alves";
+    public static String _ANTONIO_MELO     = "Antônio Melo de Oliveira";
+    public static String _BERNARDO         = "Bernardo Pinheiro de Alvarenga";
+    public static String _CACILDA          = "Cacilda de Jesus Ribeiro";
+    public static String _CARLOS_GALVAO    = "Carlos Galvão Pinheiro Júnior";
+    public static String _CASSIO_VINHAL    = "Cássio Dener Noronha Vinhal";
+    public static String _COLEMAR          = "Colemar Arruda";
+    public static String _DEMOSTENES       = "Demostenes Ferreira Filho";
+    public static String _EMILSON          = "Emilson Rocha de Oliveira";
+    public static String _ENES             = "Enes Gonçalves Marra";
+    public static String _EULER            = "Euler Bueno dos Santos";
+    public static String _FERNANDO_MELO    = "Fernando Ferreira de Melo";
+    public static String _FLAVIO           = "Flávio Henrique Teles Vieira";
+    public static String _GELSON_BRIGATTO  = "Gelson Antônio A. Brigatto";
+    public static String _GELSON_CRUZ      = "Gélson da Cruz Júnior";
+    public static String _GETULIO          = "Getúlio Antero de Deus Júnior";
+    public static String _GEYVERSON        = "Geyverson Teixeira de Paula";
+    public static String _GISELE           = "Gisele Guimarães";
+    public static String _IGOR_KOPCAK      = "Igor Kopcak";
+    public static String _JOAO_PAULO       = "João Paulo da Silva Fonseca";
+    public static String _JOSE_WILSON      = "José Wilson Lima Nerys";
+    public static String _KARINA           = "Karina Rocha Gomes da Silva";
+    public static String _KLEBER_FIGUEIREDO= "Kléber Mendes de Figueiredo";
+    public static String _LEONARDO_BRITO   = "Leonardo da Cunha Brito";
+    public static String _LEONARDO_QUEIROZ = "Leonardo de Queiroz Moreira";
+    public static String _LEONARDO_GUEDES  = "Leonardo Guerra de Rezende Guedes";
+    public static String _LINA_PAOLA       = "Lina Paola Garces Negrete";
+    public static String _LOURENCO         = "Lourenço Matias";
+    public static String _MARA_GRACE       = "Mara Grace Silva Figueiredo";
+    public static String _MARCELO_STEHLING = "Marcelo Stehling de Castro";
+    public static String _MARCO_ASSFALK    = "Marco Antônio Assfalk de Oliveira";
+    public static String _MARCOS_SOUZA     = "Marcos Antonio de Sousa";
+    public static String _MARLIPE          = "Marlipe Garcia Fagundes Neto";
+    public static String _REINALDO         = "Reinaldo Gonçalves Nogueira";
+    public static String _RODRIGO_LEMOS    = "Rodrigo Pinto Lemos";
+    public static String _ROSANGELA        = "Rosângela Nunes A. de Castro";
+    public static String _SANDRERLEY       = "Sandrerley Ramos Pires";
+    public static String _SERGIO_FIGUEIREDO= "Sérgio Araújo de Figueiredo";
+    public static String _SERGIO_GRANATO   = "Sérgio Granato de Araújo";
+    public static String _SERGIO_PIMENTEL  = "Sérgio Pires Pimentel";
+    public static String _SIGIO            = "Sigeo Kitatani Júnior";
+    public static String _THYAGO_MARQUES   = "Thyago Carvalho Marques";
+    public static String _TOMAS_BADAN      = "Tomás Antônio Costa Badan";
+    public static String _WANDER           = "Wander Gonçalves da Silva";
+    public static String _WANIR            = "Wanir José Medeiros Júnior";
+    public static String _WEBER            = "Weber Martins";
+    
+    
+}
