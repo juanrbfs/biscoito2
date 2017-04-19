b929e6f2eeeb5a89bb5c76eb453046feacb53169
PMec Humanoide <PMec Humanoide@PMecHumanoide>
2017-04-19 16:20:34 -0300

    2 commit

--- Timetabling/src/timetabling/Testes/MascModel.java
+++ Timetabling/src/timetabling/Testes/MascModel.java
@@ -14,76 +14,113 @@ package timetabling.Testes;
 //para soluções unicas
 public class MascModel {
   
-    int classroom[][]; //mascara de salas-horario
-    int teachers [][]; // mascara de professore-horario
-    int _classes [][]; // mascara de disciplinas-horario
-    int student [][];  // mascara de estudante-horario
-    int classoomCapacity[][];
+    boolean classroom[][]; //mascara de salas-horario
+    boolean teachers [][]; // mascara de professore-horario
+    boolean students [][];  // mascara de estudante-horario
+    boolean classoomCapacity[][];//mascara de capacidade das salas
     
-    int n_times; 
-    int n_classes;  
-    int n_classrooms;
-    int n_teachers;
-    int n_student; 
-    int n_classoomCapacity;
+    int n_times; // numero de timeslots
+    int n_classrooms;//numero de salas
+    int n_teachers;//numero de professores
+    int n_student; //numero de estudantes
+    int n_classoomCapacity;//capacidade das salas
     
-    
+    //Mascara de soluçoes estrutura que contrem todas as mascaras 
     Object objMask[];
     
-    //inicializa com timeslots, numero de disciplinas, numero de salas e numero de professores
-    MascModel(int n_times,int n_classes,int n_classrooms,int n_teachers,int n_student, int n_classoomCapacity){
+    //inicializa com timeslots, numero de salas e numero de professores
+    MascModel(int n_times,int n_classrooms,int n_teachers,int n_student, int n_classoomCapacity){
         
+        //tamanho da matriz
         this.n_times            = n_times; 
-        this.n_classes          = n_classes;  
         this.n_classrooms       = n_classrooms;
         this.n_teachers         = n_teachers;
         this.n_student          = n_student; 
         this.n_classoomCapacity = n_classoomCapacity;
         
-         classroom = new int[n_classrooms][n_times]; 
-         teachers  = new int[n_teachers][n_times]; 
-         _classes  = new int[n_classes][n_times]; 
-         student  = new int[n_student][n_times]; 
-         classoomCapacity = new int[n_classrooms][n_classoomCapacity];
+        //matrizes
+         classroom = new boolean[n_classrooms][n_times];                   //sala - timeslot
+         teachers  = new boolean[n_teachers][n_times];                     //professores - timeslot
+         students  = new boolean[n_student][n_times];                       //estudante - timeslot
+         classoomCapacity = new boolean[n_classrooms][n_classoomCapacity]; // sala - capacidade
          
          initMask();
          
          objMask = new Object[5];
          objMask[0] = classroom;
          objMask[1] = teachers;
-         objMask[2] = _classes;
-         objMask[3] = student;
-         objMask[4] = classoomCapacity;
+         objMask[2] = students;
+         objMask[3] = classoomCapacity;
          
     }
     
-    //iniciar a marcara de validação de schedule
+    //iniciar a marcara de validação de schedule todos as matrizes
     public void initMask(){
         
         for (int i = 0; i < n_times; i++) {
-            for (int j = 0; j < n_classes; j++) {
-                classroom[i][j] = 0;
+            for (int j = 0; j < n_classrooms; j++) {
+                classroom[i][j] = false;
             }
         }
         
        for (int i = 0; i < n_times; i++) {
             for (int j = 0; j < n_teachers; j++) {
-                teachers[i][j] = 0;
+                teachers[i][j] = false;
             }
         }
-        
-        for (int i = 0; i < n_times; i++) {
-            for (int j = 0; j < n_classrooms; j++) {
-                _classes[i][j] = 0;
-            }
-        }
-        
+            
         for (int i = 0; i < n_times; i++) {
             for (int j = 0; j < n_student; j++) {
-                student[i][j] = 0;
+                students[i][j] = false;
+            }
+        }
+        
+        for (int i = 0; i < n_classrooms; i++) {
+            for (int j = 0; j < n_classoomCapacity; j++) {
+                classoomCapacity[i][j] = false;
             }
         }
     }
     
+    //vector timeslot,sala,professor,estudante,capacidade
+    public void setValue(int vector[]){
+        int timeslot = vector[0];
+        int classromm = vector[1];
+        int teacher = vector[2];
+        int student = vector[3];
+        int capacity = vector[4];
+        
+        classroom[classromm][timeslot] = true; 
+        teachers[teacher][timeslot]  = true; 
+        students[student][timeslot]  = true; 
+        classoomCapacity[classromm][capacity]= true; 
+    }
+    
+    //retorna se o ponto já esta colocado na mascara de solução
+    //e seta esse ponto na mascara de solução
+    public boolean isValidValue(int vector[]){
+        boolean value = false;
+        int timeslot = vector[0];
+        int classromm = vector[1];
+        int teacher = vector[2];
+        int student = vector[3];
+        int capacity = vector[4];
+        
+        if(classroom[classromm][timeslot] != true )
+            value = true;
+        if(teachers[teacher][timeslot]  != true)
+            value = true;
+        if(students[student][timeslot]  == true)
+            value = true;
+        if(classoomCapacity[classromm][capacity]== true)
+            value = true;
+        
+        if(value == true){
+            setValue(vector);
+            return true;
+        }
+        
+        return false;
+    }
     
 }
